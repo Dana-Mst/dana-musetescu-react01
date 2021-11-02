cdb4ffb1161d5a272d547ac70022358909127490
danamusetescu <danactt27@gmail.com>
2021-11-02 20:06:09 +0200

    common folder with footer and header created

---
+++ generic-game/src/App.jsx
@@ -0,0 +1,13 @@
+import { Footer, Header } from './components/common';
+
+export const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <main>app goes here</main>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default App;
